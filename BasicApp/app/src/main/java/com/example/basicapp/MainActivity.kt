a93f7771d3ecd3e65325e8a8f663504fb5b3ae77
David Diamanta <dantadiamanta@gmail.com>
2023-11-09 16:01:01 +0700

    design the Text and bg color

--- BasicApp/app/src/main/java/com/example/basicapp/MainActivity.kt
+++ BasicApp/app/src/main/java/com/example/basicapp/MainActivity.kt
@@ -3,12 +3,14 @@ package com.example.basicapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.basicapp.ui.theme.BasicAppTheme
 
@@ -22,7 +24,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting("David")
                 }
             }
         }
@@ -31,13 +33,16 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+
+    Surface(color = Color.DarkGray) {
+        Text(text = "your name is $name, right?", color = Color.White)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     BasicAppTheme {
-        Greeting("Android")
+        Greeting("David")
     }
 }
